d2e6a61fedf7bf16825a5019adc6a02abb301460
HumaAnwar <huma01122@gmail.com>
2024-06-14 15:02:07 -0700

    splash Screen Done

--- app/src/main/java/com/hiskytechs/translate360/Ui/MainActivity.kt
+++ app/src/main/java/com/hiskytechs/translate360/Ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.hiskytechs.translate360.Ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.hiskytechs.translate360.R
+import com.hiskytechs.translate360.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+     binding=DataBindingUtil.setContentView(this@MainActivity,R.layout.activity_main)
     }
 }
